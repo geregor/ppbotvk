8325cc303fb962af2e0bc2af37b28ad3d784b702
geregor <52245105+geregor@users.noreply.github.com>
2020-06-03 11:20:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ soup = BS ( r.content , 'html.parser' )
 
 #webbrowser.open(acc, new=0, autoraise=True)
 
-chromedriver = "/geregor/ppbotvk/chromedriver"
+chromedriver = "/app/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chromedriver)
 #driver = webdriver.Chrome()
